e7fdc88aab94910776779261ef50b7ea9b4a6871
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2024-07-24 19:25:10 +0500

    add List

--- src/main/java/uz/pdp/warehouseqoshimcha/service/ClientService.java
+++ src/main/java/uz/pdp/warehouseqoshimcha/service/ClientService.java
@@ -1,8 +1,13 @@
 package uz.pdp.warehouseqoshimcha.service;
 
+import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.pdp.warehouseqoshimcha.model.Client;
 import uz.pdp.warehouseqoshimcha.repository.ClientRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -10,6 +15,19 @@ public class ClientService {
     @Autowired
     ClientRepository clientRepository;
 
+
+    public List<Client>  getAll(){
+        List<Client> clientList = clientRepository.findAll();
+        return clientList;
+
+    }
+
+    public Client getBYIDClient(Integer id){
+        Optional<Client> clientOptional = clientRepository.findById(id);
+        return clientOptional.get();
+    }
+
+    // create
 
 
 
